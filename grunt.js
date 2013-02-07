34b529194b40824a830a5347fd88f0612e53f50f
msecret <msegreto@miceover.com>
2013-02-06 21:53:11 -0800

    Created grunt file and package.json with correct options and directory structure

---
+++ grunt.js
@@ -0,0 +1,68 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    meta: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    },
+    lint: {
+      files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    concat: {
+      dist: {
+        src: [
+          '<banner:meta.banner>',
+          '<file_strip_banner:src/main.js>'
+        ],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint'
+    },
+    jshint: {
+      options: {
+        bitwise: true,
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        debug: true,
+        eqnull: true,
+        multistr: true
+      },
+      globals: {
+        browser: true,
+        dojo: true
+      }
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint qunit concat min');
+
+};
