a429644bf2f52f48526e77d0c93ee4185ebcc404
Marco Segreto <msegreto@minted0256.local>
2013-03-23 13:16:29 -0700

    Setup for pooled class
    Initial setup for the pooled object class, which would be its own
    class. Its using a new method of keeping the file together by using
    a asef, and has its own log. I may use this for all classes.
    
    Other changes:
    - Ran full build

--- dist/aronnax.js
+++ dist/aronnax.js
@@ -2387,6 +2387,33 @@ aronnax.Logger.getLog = function(name) {
  */
 
 /**
+ * @file Holds the pooled class 
+ */
+
+goog.provide('aronnax.Pooled');
+goog.require('aronnax.accessor');
+goog.require('aronnax.Logger');
+
+(function(goog, aronnax) {
+
+var _log = aronnax.Logger.getLog('pooled');
+/**
+ * A class that provides object pooling 
+ * @class
+ * @constuctor
+ * @this aronnax.Pooled
+ * @mixes aronnax.accessor
+ */
+aronnax.Pooled = function() {
+};
+
+})(goog, aronnax);
+/**
+ * Copyright (C) 2013 Marco Segreto
+ * vim: set et ts=2 sw=2 tw=80:
+ */
+
+/**
  * @file Holds the linked list and node classes
  */
 
---
+++ src/pooled.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) 2013 Marco Segreto
+ * vim: set et ts=2 sw=2 tw=80:
+ */
+
+/**
+ * @file Holds the pooled class 
+ */
+
+goog.provide('aronnax.Pooled');
+goog.require('aronnax.accessor');
+goog.require('aronnax.Logger');
+
+(function(goog, aronnax) {
+
+var _log = aronnax.Logger.getLog('pooled');
+/**
+ * A class that provides object pooling 
+ * @class
+ * @constuctor
+ * @this aronnax.Pooled
+ * @mixes aronnax.accessor
+ */
+aronnax.Pooled = function() {
+};
+
+})(goog, aronnax);
