f0a3569cfca559bd8756bccb22f57f708bac8fb6
msecret <msegreto@miceover.com>
2013-11-05 09:25:33 -0800

    Committing to move work to different computer

--- test/entity_test.js
+++ test/entity_test.js
@@ -34,8 +34,10 @@ describe('aronnax.Entity', function() {
         'testProtoEntity'
       );
 
-      expect(testProtoEntity.create).toBeDefined();
-      expect(testProtoEntity.create).toBeAFunction();
+      expect(testProtoEntity.init).toBeDefined();
+      expect(testProtoEntity.init).toBeAFunction();
+      expect(testProtoEntity.initComponents).toBeDefined();
+      expect(testProtoEntity.initComponents).toBeAFunction();
       expect(testProtoEntity.components).toBeDefined();
       expect(testProtoEntity.components).toBeAnArray();
       expect(testProtoEntity.componentList).toBeDefined();
@@ -49,30 +51,45 @@ describe('aronnax.Entity', function() {
     });
 
     it('should set the component as the component className', function() {
-      var testComponentProto = Base.create(Object.prototype, 'TestComponent', {
-            testMethod1: function() { }
+      var testComponentProto = Base.create(Object.prototype, 'TestComponentA', {
+            testMethodA: function() { },
+            init: function() { }
           }),
           TestEntityProto = Entity.create('TestEntity', [testComponentProto]),
           testInst = TestEntityProto.make();
 
-      expect(testInst.TestComponent).toBeDefined();
-      expect(testInst.TestComponent).toBe(testComponentProto);
+      expect(testInst.TestComponentA).toBeDefined();
+      expect(testInst.TestComponentA).toBe(testComponentProto);
     });
 
     it('should create add the component name to the componentList', function() {
-      var testComponentProto = Base.create(Object.prototype, 'TestComponent', {
-            testMethod1: function() { }
+      var testComponentProto = Base.create(Object.prototype, 'TestComponentB', {
+            init: function() { },
+            testMethodB: function() { }
           }),
           TestEntityProto = Entity.create('TestEntity', [testComponentProto]),
           testInst = TestEntityProto.make();
 
       expect(testInst.componentList).toBeDefined();
-      expect(testInst.componentList[0]).toEqual('TestComponent');
+      expect(testInst.componentList[0]).toEqual('TestComponentB');
     });
   });
 
   describe('addMixins', function() {
     it('should copy the properties passed in onto the Entity proto', function() {
+      var testProtoEntity = Entity.create(
+            'testProtoEntity'
+          ),
+          testMixins = {
+            testMixin: 'testString'
+          };
+
+      Entity.addMixins(testProtoEntity, testMixins);
+
+      expect(testProtoEntity.testMixin).toBeDefined();
+      expect(testProtoEntity.testMixin).toBe(testMixins.testMixin);
+    });
+    it('should have the same property on the entity inst', function() {
       var testProtoEntity = Entity.create(
             'testProtoEntity'
           ),
@@ -88,6 +105,22 @@ describe('aronnax.Entity', function() {
   });
 
   describe('initComponents', function() {
+    it('should run the init method of the components', function() {
+      var testComponentProto = Base.create(Object.prototype, 'TestComponent', {
+            x: {
+              value: 0,
+              writable: true
+            },
+            init: function(opts) { this.x = opts.x; console.log('TestComponent.init'); },
+            testMethodA: function() { }
+          }),
+          TestEntityProto = Entity.create('TestEntity', [testComponentProto]),
+          testInst = TestEntityProto.make();
+
+      testInst.init({x: 1});
+      console.dir(testInst);
+      expect(testInst.TestComponent.x).toEqual(1);
+    });
 
   });
 });
